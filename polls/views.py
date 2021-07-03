3a101c637c09a21e9c575e011a6c6b3d728349f5
ouyuru <betty9895@gmail.com>
2021-07-03 13:35:06 +0800

    add different page in the same app

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,16 @@
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader
 
 def index(request):
-    return HttpResponse('Helllo Django by HttpResponse')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
 # 建立3個view
 # detail => 你在哪一個問題頁面
 # results => 查看問題的結果
